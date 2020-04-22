a3c5e6e13e4ac2db9b6067fff71d482bf5c32fd4
Utkarsh Goel <ugoel911@gmail.com>
2020-04-22 14:08:22 +0530

    Updates

---
+++ precache-manifest.4a0647fedf591fb02e76e5bf5c1e9139.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964139cc259a5feeec0bbb41c5a86dea",
+    "url": "/Utkarsh/index.html"
+  },
+  {
+    "revision": "48303f551f6487da8209",
+    "url": "/Utkarsh/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "dd4ace20672f3ab816cf",
+    "url": "/Utkarsh/static/css/main.5a5f748b.chunk.css"
+  },
+  {
+    "revision": "48303f551f6487da8209",
+    "url": "/Utkarsh/static/js/2.8940aa31.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Utkarsh/static/js/2.8940aa31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4ace20672f3ab816cf",
+    "url": "/Utkarsh/static/js/main.2baa1f7d.chunk.js"
+  },
+  {
+    "revision": "5843839d6b2e9e6356fe",
+    "url": "/Utkarsh/static/js/runtime-main.b2a04610.js"
+  },
+  {
+    "revision": "41cd15cb855b4c6d3dae50958737af90",
+    "url": "/Utkarsh/static/media/yoal-desurmont-jqgsM3B9Fpo-unsplash.41cd15cb.jpg"
+  }
+]);
